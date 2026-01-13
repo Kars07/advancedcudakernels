8eb487ead0f9d8d1cffc231b6334282da1dfd427
Kars07 <eniaiyejuni.kayode@gmail.com>
2026-01-13 15:30:08 +0100

    Encode

---
+++ TMA/Encoding/encodemapondevice.cu
@@ -0,0 +1,60 @@
+/*
+This section explains how to encode a tiled-type tensor map on device. This is useful in situations where
+the typical way of transferring the tensor map (using const __grid_constant__ kernel parameters)
+is undesirable, for instance, when processing a batch of tensors of various sizes in a single kernel
+launch.
+The recommended pattern is as follows:
+1. Create a tensor map “template”, template_tensor_map, using the Driver API on the host.
+2. In a device kernel, copy the template_tensor_map, modify the copy, store in global memory,
+and appropriately fence.
+3. Use the tensor map in a kernel with appropriate fencing.
+The high-level code structure is as follows:
+
+*/
+
+// Initialize device context:
+CUDA_CHECK(cudaDeviceSynchronize());
+
+// Create a tensor map template using the cuTensorMapEncodeTiled driver function
+CUtensorMap template_tensor_map = make_tensormap_template();
+
+// Allocate tensor map and tensor in global memory
+CUtensorMap* global_tensor_map;
+CUDA_CHECK(cudaMalloc(&global_tensor_map, sizeof(CUtensorMap)));
+char* global_buf;
+CUDA_CHECK(cudaMalloc(&global_buf, 8 * 256));
+
+// Fill global buffer with data.
+fill_global_buf<<<1, 1>>>(global_buf);
+
+// Define the parameters of the tensor map that will be created on device.
+tensormap_params p{};
+p.global_address = global_buf;
+p.rank = 2;
+p.box_dim[0] = 128; // The box in shared memory has half the width of the full buffer
+p.box_dim[1] = 4; // The box in shared memory has half the height of the full buffer
+p.global_dim[0] = 256; //
+p.global_dim[1] = 8; //
+p.global_stride[0] = 256; //
+p.global_stride[1] = 256; //
+p.element_stride[0] = 1; //
+p.element_stride[1] = 1; //
+
+// Encode global_tensor_map on device:
+encode_tensor_map<<<1, 32>>>(template_tensor_map, p, global_tensor_map);
+
+// Use it from another kernel:
+consume_tensor_map<<<1, 1>>>(global_tensor_map);
+
+// Check for errors:
+CUDA_CHECK(cudaDeviceSynchronize());
+
+struct tensormap_params {
+    void* global_address;
+    int rank;
+    uint32_t box_dim[5];
+    uint64_t global_dim[5];
+    size_t global_stride[4];
+    uint32_t element_stride[5];
+
+};
